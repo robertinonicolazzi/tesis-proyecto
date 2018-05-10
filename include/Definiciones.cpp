2a09047eaedeeac94adf2bcad366268cc235f41d
Robertino Nicolazzi <robertinonicolazzi@gmail.com>
2018-05-10 09:21:51 -0300

    inicial

---
+++ include/Definiciones.cpp
@@ -0,0 +1,13 @@
+#include <Definiciones.h>
+
+
+
+std::string ST_BASE_SHADER = "resources/shaders/";
+std::string VS_LIGHT_SHADER = ST_BASE_SHADER + "light.vs";
+std::string FS_LIGHT_SHADER = ST_BASE_SHADER + "light.fs";
+
+std::string VS_PLAYER_SHADER = ST_BASE_SHADER + "player.vs";
+std::string FS_PLAYER_SHADER = ST_BASE_SHADER + "player.fs";
+
+
+
---
+++ include/Definiciones.h
@@ -0,0 +1,27 @@
+/*
+ * Definiciones.h
+ *
+ *  Created on: 8/1/2018
+ *      Author: robertnn
+ */
+
+#ifndef DEFINICIONES_H_
+#define DEFINICIONES_H_
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/string_cast.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <math.h>
+
+
+
+
+
+#endif /* DEFINICIONES_H_ */
